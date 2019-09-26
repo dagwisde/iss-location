08b91872881ab14a4c827a01f0a2637534a79808
Oshadowah <dagwisde@gmail.com>
2019-09-25 20:41:12 -0400

    Initialize the Leaflet map

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
 // API url variables
 const stationApi = 'https://api.wheretheiss.at/v1/satellites/25544';
 const spacePeopleAPi = 'http://api.open-notify.org/astros.json';
+
+// Initialize the Leaflet map
+const stationMap = L.map('stationMap');
